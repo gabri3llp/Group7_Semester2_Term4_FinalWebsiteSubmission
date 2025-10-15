0a578dddf0747c767bf6cda621b45d8b0da9c49e
Larissa Heyns <heynslarissa@gmail.com>
2025-10-15 14:35:00 +0200

    Update Script.js

---
+++ js/Script.js
@@ -0,0 +1,15 @@
+!async function(){
+const url = 'https://movies-api14.p.rapidapi.com/shows';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '8e9074276emsh223ef54cf19b760p19a968jsn3a988955cdbc',
+		'x-rapidapi-host': 'movies-api14.p.rapidapi.com'
+	}
+};
+
+let data= await fetch(url,options)
+	.then ((response)=>response.json())
+	.then((result)=>{return result})
+	.catch((error)=> console.error(error));
+}();
