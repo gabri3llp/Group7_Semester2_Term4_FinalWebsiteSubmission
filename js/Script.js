1bacd4c9a256a3dc2ba3abad72992a3243305db0
Larissa Heyns <heynslarissa@gmail.com>
2025-10-16 15:17:15 +0200

    Api again

--- js/Script.js
+++ js/Script.js
@@ -1,10 +1,10 @@
 !async function(){
-const url = 'https://movies-api14.p.rapidapi.com/shows';
+const url = 'https://imdb238.p.rapidapi.com/imdb/tt2911666';
 const options = {
 	method: 'GET',
 	headers: {
 		'x-rapidapi-key': '8e9074276emsh223ef54cf19b760p19a968jsn3a988955cdbc',
-		'x-rapidapi-host': 'movies-api14.p.rapidapi.com'
+		'x-rapidapi-host': 'imdb238.p.rapidapi.com'
 	}
 };
 
@@ -13,6 +13,6 @@ const options = {
         .then((result) => {return result})
         .catch((error)=> console.error(error));
 
-        console.log(data);
+        console.log(data[2]);
       
 }();
