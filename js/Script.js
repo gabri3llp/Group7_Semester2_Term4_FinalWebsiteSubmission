cfe8e09eeb16809cc9870a5e711a071f253f78d2
251142-coenraadjlindholm <251142@virtualwindow.co.za>
2025-10-20 21:04:45 +0200

    Added API

--- js/Script.js
+++ js/Script.js
@@ -99,3 +99,27 @@ function login(){
     console.log(systemUser.checkPassword());
   }
 }
+
+//API Added
+!async function () {
+    
+    const apiKey = '0363063badd97c1049612cbbac78bf57';
+    
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
+    };
+
+    let data = await fetch(url, options)
+        .then((response)=> response.json())
+        .then((result) => {return result})
+        .catch((error)=> console.error(error));
+
+        console.log(data);
+
+    
+}();
