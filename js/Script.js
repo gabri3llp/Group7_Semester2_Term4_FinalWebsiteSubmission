621761ba329d3f713605cb03547ce07759c1e1e3
251142-coenraadjlindholm <251142@virtualwindow.co.za>
2025-10-20 21:09:32 +0200

    Added API and linked java

--- js/Script.js
+++ js/Script.js
@@ -1,3 +1,27 @@
+//API Added
+!async function () {
+    
+    const apiKey = '0363063badd97c1049612cbbac78bf57';
+    
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
+    };
+
+    let data = await fetch(url, options)
+        .then((response)=> response.json())
+        .then((result) => {return result})
+        .catch((error)=> console.error(error));
+
+        console.log(data);
+
+    
+}();
+
 //Sign in/Sign up
 
 // Select the flip container
@@ -100,26 +124,3 @@ function login(){
   }
 }
 
-//API Added
-!async function () {
-    
-    const apiKey = '0363063badd97c1049612cbbac78bf57';
-    
-    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
-
-    const options = {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json'
-        }
-    };
-
-    let data = await fetch(url, options)
-        .then((response)=> response.json())
-        .then((result) => {return result})
-        .catch((error)=> console.error(error));
-
-        console.log(data);
-
-    
-}();
